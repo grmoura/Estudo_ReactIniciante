e6c99ceb5ba146bb7685d5e68b9795c57199559c
GRMoura55 <gabriel.rodrigues@kahsh.com>
2023-02-22 11:03:53 -0400

    feat: menu de navegação de paginas utilizando router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-//import ComponenteButton from './components/ComponenteButton'
-//import ComponenteTeste from './components/ComponenteTeste'
-//import PropsTeste from './components/PropsTeste'
-///import XPropsTypeTeste from './components/XPropsTypeTeste'
-//import Evento from './components/Evento'
-//import Condicional from './components/Condicional'
-import Renderizacao from './components/Renderizacao';
-import './App.css'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import "./App.css";
 
 function App() {
-  const texte = ["moura", "rodrigues", "gabriel"];
   return (
-    
-    <div className="App">
-      <header className="App-header">
-        <li>CONDICIONAL & RENDERIZAÇÃO</li>
-        <Renderizacao  itens={texte}/>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home </Link>
+        </li>
+        <li>
+          <Link to="/contato">Contado </Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route  path="/" element={<Home/>} exact></Route>
+        <Route  path="/contato" element={<Contato />} exact></Route>
+      </Routes>
+
+
+    </Router>
   );
 }
 
